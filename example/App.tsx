62c7ed78b6539227c1da8d5be4a2c25bd907808f
FreakyCoder <kurayogun@gmail.com>
2022-05-05 10:17:32 +0300

    feat: version 1.0.0 WIP

--- example/App.tsx
+++ example/App.tsx
@@ -11,46 +11,46 @@ const colorfyBG = require("./assets/ColorfyBG.jpg");
 const brawlers = require("./assets/hero_bg_brawlstars_.jpg");
 const profileImageSource = {
   uri:
-    "https://images.unsplash.com/photo-1514846226882-28b324ef7f28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+      "https://images.unsplash.com/photo-1514846226882-28b324ef7f28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
 };
 
 const App = () => {
   const [isInternetAvailable, setIsInternetAvailable] = useState(false);
   return (
-    <InternetConnectionAlert
-      onChange={(connectionState) => {
-        console.log("Connection State: ", connectionState);
-        setIsInternetAvailable(connectionState.isConnected);
-      }}
-    >
-      <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <GorgeousHeader
-            title="Apps"
-            subtitle="Find the best apps of the week"
-            placeholder="Find the apps"
-            menuImageSource={menuImage}
-            searchImageSource={searchImage}
-            searchBarStyle={styles.headerSearchBarStyle}
-            profileImageSource={profileImageSource}
-          />
-          <ScrollView
-            style={styles.container}
-            contentContainerStyle={styles.contentContainerStyle}
-            contentInset={styles.contentInsetStyle}
-          >
-            <AppOfTheDayCard
-              iconSource={colorfy}
-              backgroundSource={colorfyBG}
+      <InternetConnectionAlert
+          onChange={(connectionState) => {
+            console.log("Connection State: ", connectionState);
+            setIsInternetAvailable(connectionState.isConnected);
+          }}
+      >
+        <>
+          <StatusBar barStyle="dark-content" />
+          <SafeAreaView>
+            <GorgeousHeader
+                title="Apps"
+                subtitle="Find the best apps of the week"
+                placeholder="Find the apps"
+                menuImageSource={menuImage}
+                searchImageSource={searchImage}
+                searchBarStyle={styles.headerSearchBarStyle}
+                profileImageSource={profileImageSource}
             />
-            <View style={styles.appleCardContainer}>
-              <AppleCard source={brawlers} />
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-      </>
-    </InternetConnectionAlert>
+            <ScrollView
+                style={styles.container}
+                contentContainerStyle={styles.contentContainerStyle}
+                contentInset={styles.contentInsetStyle}
+            >
+              <AppOfTheDayCard
+                  iconSource={colorfy}
+                  backgroundSource={colorfyBG}
+              />
+              <View style={styles.appleCardContainer}>
+                <AppleCard source={brawlers} />
+              </View>
+            </ScrollView>
+          </SafeAreaView>
+        </>
+      </InternetConnectionAlert>
   );
 };
 
